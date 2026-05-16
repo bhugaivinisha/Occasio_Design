71a310046f988d1f6b84f578d9c85183c4a9eb76
Binisha Bhugai <bhugaivinisha@gmail.con>
2026-05-16 11:01:07 +0545

    Admin and User password

--- sql/occasio_design.sql
+++ sql/occasio_design.sql
@@ -1,3 +1,12 @@
+/* 
+Admin: bhugaivinisha@gmail.com
+password: Binisha@123
+User : binisha@gmail.com
+password: Binisha@123
+*/
+
+
+
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
